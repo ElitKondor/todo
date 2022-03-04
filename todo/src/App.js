3751f6db8e282dcaf53ea6c35a35a8004e39b904
Ruslan Miahkykh <rmiah@softserveinc.com>
2022-03-04 22:41:39 +0200

    Just a template for a future mini-project

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TodoApp from "./components/todo.component";
+
+function App() {
+    return (
+        <div className="app">
+        <TodoApp />
+        </div>
+    )
+
+}
+
+export default App;
